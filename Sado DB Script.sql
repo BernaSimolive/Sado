04b4e85ca51bc78cca82ec0711cdeaec88cb31a0
BernaSimolive <167207574+BernaSimolive@users.noreply.github.com>
2024-06-04 12:15:38 -0300

    Add files via upload

---
+++ Sado DB Script.sql
@@ -0,0 +1,104 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sado
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `doador`
+--
+
+DROP TABLE IF EXISTS `doador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `doador` (
+  `idDoador` int NOT NULL AUTO_INCREMENT,
+  `cpfDoador` varchar(11) NOT NULL,
+  `nomeDoador` varchar(60) NOT NULL,
+  `endereco` varchar(100) DEFAULT NULL,
+  `telefone` varchar(45) DEFAULT NULL,
+  `email` varchar(70) DEFAULT NULL,
+  PRIMARY KEY (`idDoador`,`cpfDoador`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doador`
+--
+
+LOCK TABLES `doador` WRITE;
+/*!40000 ALTER TABLE `doador` DISABLE KEYS */;
+INSERT INTO `doador` VALUES (1,'47870284807','Bernardo Simonet Oliveira','Av.José Giorgi 600 ','11945625606','bernardosimolive@gmail.com'),(2,'12345678910','João Paulo Santos','Curitiba','11945625606','jaosantos@gmail.com'),(3,'10987654321','Erick Soares','Vital Brasil','1199996969','Erick.Soares@gmail.com');
+/*!40000 ALTER TABLE `doador` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `doacao`
+--
+
+DROP TABLE IF EXISTS `doacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `doacao` (
+  `idDoacao` int NOT NULL AUTO_INCREMENT,
+  `nomeAlimento` varchar(45) NOT NULL,
+  `quantidade` int NOT NULL,
+  `peso` double NOT NULL,
+  `descricao` varchar(45) DEFAULT NULL,
+  `Doador_cpfDoador` varchar(11) NOT NULL,
+  PRIMARY KEY (`idDoacao`),
+  KEY `fk_Doacao_Doador_idx` (`Doador_cpfDoador`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doacao`
+--
+
+LOCK TABLES `doacao` WRITE;
+/*!40000 ALTER TABLE `doacao` DISABLE KEYS */;
+INSERT INTO `doacao` VALUES (1,'Banana',30,0.2,'Banana Nanica','47870284807'),(2,'Arroz',10,1,'Arroz tipo 1','47870284807'),(3,'Carne',3,1.2,'Picanha','47870284807'),(4,'Feijão',12,1,'Feijão Preto ','12345678910'),(5,'Água',20,2,'Água mineral ','47870284807'),(6,'Café',12,1,'Café Pelé','47870284807');
+/*!40000 ALTER TABLE `doacao` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+
